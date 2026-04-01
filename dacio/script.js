3f8377c3009747396b50e33e427afe91129f5667
Matheus-FRocha <matheusrocha063@hotmail.com>
2026-04-01 20:38:57 -0300

    Continuação

---
+++ dacio/script.js
@@ -0,0 +1,62 @@
+const produto = {
+    "123": {"nome": "Coca-Cola Espumante", "preco": 9.99},
+    "456": {"nome": "Um mamute Pequenino", "preco": 8.99},
+    "789": {"nome": "Vampiro Doidão", "preco": 4.69} 
+}
+
+let carrinho = [];
+
+const audio = new Audio("bip.mp3");
+
+window.onload = () =>{
+    document.getElementById("cod").focus();
+}
+
+function addProduto(){
+    const codValue = document.getElementById("cod");
+    const qtdValue = document.getElementById("qtdXun");
+
+    const codigo = codValue.value;
+    const quantidade = qtdValue.value;
+
+    if(!produto[codigo]){
+        AlertItem();
+        return;
+    }
+
+    const produtoBase = produto[codigo];
+    const item ={
+        nome: produtoBase.nome,
+        preco: produtoBase.preco,
+        quantidade: quantidade,
+        subtotal: produtoBase.preco * quantidade
+    };
+
+    carrinho.push(item);
+    audio.currentTime = 0;
+    audio.play();
+
+    atualizarTela();
+}
+
+function atualizarTela(){
+
+    const lista = getElementById("list");
+    lista.innerHTML = "";
+
+    let total = 0;
+
+    carrinho.forEach((item, index) => {
+        total+= item.subtotal;
+
+        const li = document.createElement("li");
+        li.className = "list-group-item";
+
+        li.innerHTML = `<div class="d-flex justify-content-between"
+            <strong>${item.nome}</strong>
+            <small> ${item.quantidade} x R$ ${item.preco} = <strong>${item.subtotal}</strong></small>
+            </div>`;
+
+        lista.appendChild(li);
+    });
+}
